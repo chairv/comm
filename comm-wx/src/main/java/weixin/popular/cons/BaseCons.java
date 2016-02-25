f55f599dbb62db2f85e5a15ef606841b3c3dca50
tancw <changwei114@126.com>
2016-02-25 17:35:26 +0800

    微信开发

--- comm-wx/src/main/java/weixin/popular/cons/BaseCons.java
+++ comm-wx/src/main/java/weixin/popular/cons/BaseCons.java
@@ -5,8 +5,9 @@ import utils.SysConf;
 /**
  * Created by tancw on 2016/2/25.
  */
-public class Base {
+public class BaseCons {
 	public static final String APPID = SysConf.get("weixin.appid");
 	public static final String APPSECRET = SysConf.get("weixin.appsecret");
 	public static final String BASE_URL = SysConf.get("base.path");
+	public static final String SESSION_USER = "session_user";
 }
