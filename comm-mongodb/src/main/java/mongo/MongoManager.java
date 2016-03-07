a2df18cbc838cf7abf55c36488e2c01af7a52bb6
tancw <changwei114@126.com>
2016-03-07 17:31:48 +0800

    微信开发

---
+++ comm-mongodb/src/main/java/mongo/MongoManager.java
@@ -0,0 +1,11 @@
+package mongo;
+
+/**
+ * Created by tancw on 2016/3/4.
+ */
+public class MongoManager {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ comm-utils/src/main/java/utils/ProsUtils.java
@@ -0,0 +1,14 @@
+package utils;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+/**
+ * Created by tancw on 2016/3/4.
+ */
+public class ProsUtils {
+	private static PropertyPlaceholderConfigurer configurer;
+
+	static {
+
+	}
+}
